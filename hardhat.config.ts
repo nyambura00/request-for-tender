3aada7c835f168616bd8c323b884158983abe441
nyambura00 <winnienyambura14@gmail.com>
2022-10-17 20:05:59 +0300

    add NFT contract

---
+++ hardhat.config.ts
@@ -0,0 +1,21 @@
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config({path: '.env'});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  networks: {
+    alfajores: {
+      url: "https://alfajores-forno.celo-testnet.org",
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+        path: "m/44'/52752'/0'/0"
+      },
+    }
+  },
+  solidity: "0.8.4",
+};
