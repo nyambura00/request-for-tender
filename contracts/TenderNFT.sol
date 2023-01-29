092ac9603742655f2a9e55ec95178a69c136e156
nyambura00 <winnienyambura14@gmail.com>
2023-01-30 00:31:56 +0300

    add transfer fn

--- contracts/TenderNFT.sol
+++ contracts/TenderNFT.sol
@@ -2,15 +2,21 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract RFT {
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract RFT is IERC721, ERC721URIStorage{
     address public owner;
     mapping(address => bool) public bidders;
     uint public bidEndTime;
     address public highestBidder;
     uint public highestBid;
 
-    constructor() {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("TenderNFT", "RFT") {
         owner = msg.sender;
         bidEndTime = block.timestamp + 1 weeks;
     }
@@ -26,10 +32,16 @@ contract RFT {
         highestBid = _bid;
     }
 
-    function award() public {
+    function award(address serviceProvider, string memory tokenURI) public returns(uint256) {
         require(msg.sender == owner, "Only the RFT owner can award the contract.");
         require(block.timestamp > bidEndTime, "Bidding period is not yet over.");
 
         // transfer NFT to the highest bidder
+        uint256 newItemId = _tokenIds.current();
+        _mint(serviceProvider, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        _tokenIds.increment();
+        return newItemId;
     }
 }
